f79e0717a79f089d663ed67227d6ec94bc25059a
wfhadit <wfhadit@gmail.com>
2023-09-14 10:21:30 +0700

    yayaya
    yayaya

--- src/pages/CashierPages/CashierLandingPage.jsx
+++ src/pages/CashierPages/CashierLandingPage.jsx
@@ -1,13 +1,49 @@
+// import { Container } from "react-bootstrap";
+// import { Header } from "../../components/Header";
+
+// export const CashierLandingPage = () => {
+//   return (
+//     <div>
+//       <Header />
+//       <Container>
+//         <h1>Hellow</h1>
+//       </Container>
+//     </div>
+//   );
+// };
+
 import { Container } from "react-bootstrap";
 import { Header } from "../../components/Header";
+import { Flex, useDisclosure, Center } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { ProductList } from "../../components/product";
+import add50 from "../../assets/icons8-plus.svg";
+import { ModalInputProduct } from "../../components/modal";
+import { api } from "../../API/api";
 
-export const CashierLandingPage = () => {
+export const CashierLandingPage = ({ search }) => {
+  const [products, setProducts] = useState([]);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const fetchProducts = async () => {
+    try {
+      const res = await api.get("/products", {
+        params: { product_name: search },
+      });
+      setProducts([...res.data]);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, [search]);
   return (
-    <div>
-      <Header />
-      <Container>
-        <h1>Hellow</h1>
-      </Container>
-    </div>
+    <>
+      <Center alignItems={"flex-start"} marginTop={"35px"}>
+        <ProductList products={[...products]} fetchProducts={fetchProducts} />
+      </Center>
+    </>
   );
 };
