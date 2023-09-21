0c0ad9a1cd1926dfd2638d1ea1fd1208d3cf0324
wfhadit <wfhadit@gmail.com>
2023-09-21 09:40:14 +0700

    add

--- src/components/post/post-card.jsx
+++ src/components/post/post-card.jsx
@@ -1,31 +1,18 @@
-import { api } from '../../API/api';
-import { DeleteIcon, EditIcon } from '@chakra-ui/icons';
-import { useSelector } from 'react-redux';
-import { useDisclosure, useToast } from '@chakra-ui/react';
-import { ModalInputProduct } from './post-modal';
+import { api } from "../../API/api";
+import { DeleteIcon, EditIcon } from "@chakra-ui/icons";
+import { useSelector } from "react-redux";
+import { useDisclosure, useToast } from "@chakra-ui/react";
+import { ModalInputProduct } from "./post-modal";
 
 export const PostCard = ({ product, fetchProducts }) => {
   const userSelector = useSelector((state) => state.auth);
   const disclosure = useDisclosure();
   const toast = useToast();
 
-  const categoryName = (categoryId) => {
-    switch (categoryId) {
-      case 1:
-        return 'Coffee';
-      case 2:
-        return 'Non-Coffee';
-      case 3:
-        return 'Pizza';
-      default:
-        return '';
-    }
-  };
-
   const deleteProduct = (productId) => {
-    const token = localStorage.getItem('cs-token');
+    const token = localStorage.getItem("cs-token");
 
-    const isConfirmed = window.confirm('Are you sure you want to delete this?');
+    const isConfirmed = window.confirm("Are you sure you want to delete this?");
     if (!isConfirmed) {
       return;
     }
@@ -33,25 +20,25 @@ export const PostCard = ({ product, fetchProducts }) => {
     api
       .delete(`/products/${productId}`, {
         headers: {
-          Authorization: 'Bearer ' + localStorage.getItem('cs-token'),
-          'api-key': userSelector?.username,
+          Authorization: "Bearer " + localStorage.getItem("cs-token"),
+          "api-key": userSelector?.username,
         },
       })
       .then(() => {
         toast({
-          title: 'Product has been deleted',
-          status: 'success',
+          title: "Product has been deleted",
+          status: "success",
           isClosable: true,
-          position: 'top',
+          position: "top",
           duration: 3000,
         });
         fetchProducts();
       })
       .catch((err) => {
         toast({
-          title: 'Delete product failed',
-          status: 'error',
-          position: 'top',
+          title: "Delete product failed",
+          status: "error",
+          position: "top",
           isClosable: true,
           duration: 1500,
         });
@@ -62,42 +49,42 @@ export const PostCard = ({ product, fetchProducts }) => {
     <>
       <tr
         key={product.id}
-        className='hover:bg-slate-300 transform hover:scale-105 hover:shadow-md'
+        className="hover:bg-slate-300 transform hover:scale-105 hover:shadow-md"
       >
         <ModalInputProduct
           product={product}
           isOpen={disclosure.isOpen}
           onClose={disclosure.onClose}
-          edit={'edit'}
+          edit={"edit"}
           fetchProducts={fetchProducts}
         />
-        <td className='border px-4 py-2'>{product.id}</td>
-        <td className='border px-4 py-2'>
+        <td className="border px-4 py-2">{product.id}</td>
+        <td className="border px-4 py-2">
           <img
             src={`http://localhost:2500/public/product/${product.imageName}`}
-            width='100'
-            height='100'
-            alt=''
-            objectFit='cover'
+            width="100"
+            height="100"
+            alt=""
+            objectFit="cover"
           />
         </td>
-        <td className='border px-4 py-2'>{product.productName}</td>
-        <td className='border px-4 py-2'>{categoryName(product.categoryId)}</td>
-        <td className='border px-4 py-2'>Rp. {product.price}</td>
-        <td className='border px-4 py-2'>{product.stock}</td>
-        <td className='border px-4 py-2'>{product.desc}</td>
-        <td className='border px-4 py-2'>
+        <td className="border px-4 py-2">{product.productName}</td>
+        <td className="border px-4 py-2">{product.Category.category_name}</td>
+        <td className="border px-4 py-2">Rp. {product.price}</td>
+        <td className="border px-4 py-2">{product.stock}</td>
+        <td className="border px-4 py-2">{product.desc}</td>
+        <td className="border px-4 py-2">
           <EditIcon
             boxSize={5}
-            cursor={'pointer'}
+            cursor={"pointer"}
             onClick={() => disclosure.onOpen()}
           />
         </td>
-        <td className='border px-4 py-2'>
+        <td className="border px-4 py-2">
           <DeleteIcon
-            color={'red.500'}
+            color={"red.500"}
             boxSize={5}
-            cursor={'pointer'}
+            cursor={"pointer"}
             onClick={() => deleteProduct(product.id)}
           />
         </td>
